e407e0a244204146c6925d324b9ee3ab34a6cdfe
amy-pak <lihm2@naver.com>
2022-12-14 19:38:20 +0900

    refactor: change method name create->insert

--- src/main/java/com/zpop/web/dao/CommentDao.java
+++ src/main/java/com/zpop/web/dao/CommentDao.java
@@ -12,15 +12,15 @@ public interface CommentDao {
 		/* groupId : 1depth아이디 */
 		
 		List<Comment> getList(int meetingId);
-		int createComment(int meetingId, int writerId, String text); //댓글
-		int createComment(Map map);
-		int createReply(int meetingId, int parentId, int writerId, String text); //대댓글
-		int createReply(Map map);
+		int insertComment(int meetingId, int writerId, String text); //댓글
+		int insertComment(Map map);
+		int insertReply(int meetingId, int parentId, int writerId, String text); //대댓글
+		int insertReply(Map map);
 		
 		/*
 		 * 대댓글에 대한 댓글의 경우 직계의 parentId를 통해 depth1의 조부모Id값을 얻을 수 있으므로 오버로딩 생략.
 		 */
-		int updateComment(int id);
+		int updateComment(Comment cmt);
 		int deleteComment(int id);
 
 
