68dc5634c9d87492580429b9849edf4455c1087a
dladnqls222 <69413567+dladnqls222@users.noreply.github.com>
2022-12-15 15:33:37 +0900

    change path of dao interface

--- src/main/java/com/zpop/web/dao/ReportedMemberDao.java
+++ src/main/java/com/zpop/web/dao/ReportedMemberDao.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.zpop.entity.Notification;
+import com.zpop.web.entity.Notification;
 
 @Mapper
 public interface ReportedMemberDao {
