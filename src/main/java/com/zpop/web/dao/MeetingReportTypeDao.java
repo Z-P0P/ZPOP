7c89e7b1ce8844844bd31ff5326582897e94fef6
dladnqls222 <69413567+dladnqls222@users.noreply.github.com>
2022-12-15 15:29:28 +0900

    add report and notification dao files

---
+++ src/main/java/com/zpop/web/dao/MeetingReportTypeDao.java
@@ -0,0 +1,12 @@
+package com.zpop.web.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.zpop.entity.Notification;
+
+@Mapper
+public interface MeetingReportTypeDao {
+	
+}
