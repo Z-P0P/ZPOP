f4baac793aaec15ecbbd0ad5e8e66f969560df74
dladnqls222 <69413567+dladnqls222@users.noreply.github.com>
2022-12-19 20:01:43 +0900

    feat : add member report files

--- src/main/java/com/zpop/web/dao/ReportedMemberDao.java
+++ src/main/java/com/zpop/web/dao/ReportedMemberDao.java
@@ -1,12 +1,12 @@
 package com.zpop.web.dao;
 
-import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
 
-import com.zpop.web.entity.Notification;
+import com.zpop.web.entity.ReportedMember;
 
 @Mapper
 public interface ReportedMemberDao {
+	
+	int insert(ReportedMember reportedMember);
 	
 }
