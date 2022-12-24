068bd8ac75c1cac4b150ee7d05ba3adbc97eeec6
jin <apple380g@gmail.com>
2022-12-24 23:36:11 +0900

    #14  memberDao와 MemberDaoMapper에 myMeetingView 적용 (test완료)

--- src/main/java/com/zpop/web/dao/MemberDao.java
+++ src/main/java/com/zpop/web/dao/MemberDao.java
@@ -1,13 +1,10 @@
 package com.zpop.web.dao;
 
 import com.zpop.web.entity.Member;
+import com.zpop.web.entity.member.MyMeetingView;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
-
-import org.apache.ibatis.annotations.Mapper;
-
-import com.zpop.web.entity.Member;
 
 @Mapper
 public interface MemberDao {
@@ -26,4 +23,6 @@ public interface MemberDao {
 	int update(Member member);
 
 	int count(int socialTypeId);
+
+	List<MyMeetingView> getMyMeeting(int id);
 }
