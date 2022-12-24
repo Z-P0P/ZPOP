c30b2f36e46439a015fc620efdc963c87d9a82c5
Kyunghwan Kim <kkh147.17.3@gmail.com>
2022-12-25 00:51:27 +0900

    feat: add memberEval & participation listing

--- src/main/java/com/zpop/web/service/admin/DeafultAdminMemberService.java
+++ src/main/java/com/zpop/web/service/admin/DeafultAdminMemberService.java
@@ -6,32 +6,67 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.zpop.web.dao.MemberDao;
-import com.zpop.web.dto.admin.AdminMemberDto;
+import com.zpop.web.dao.MemberEvalDao;
+import com.zpop.web.dao.ParticipationDao;
+import com.zpop.web.dto.admin.AdminMemberEvalDto;
+import com.zpop.web.dto.admin.AdminParticipationDto;
 import com.zpop.web.entity.Member;
-import com.zpop.web.service.MemberService;
 
 @Service
 public class DeafultAdminMemberService implements AdminMemberService{
 
 	private final MemberDao memberDao;
+	private final MemberEvalDao memberEvalDao;
+	private final ParticipationDao participationDao;
 	
 	@Autowired
-	public DeafultAdminMemberService(MemberDao memberDao) {
+	public DeafultAdminMemberService(MemberDao memberDao
+									,MemberEvalDao memberEvalDao
+									,ParticipationDao participationDao) {
 		this.memberDao = memberDao;
+		this.memberEvalDao =memberEvalDao;
+		this.participationDao = participationDao;
 	}
 	
 	@Override
 	public List<Member> getList(int page, String keyword, String option) {
 		int size = 10;
 		int offset=(page-1)*size;
-		List<Member> list = memberDao.getList(size, offset, null, null);
+		List<Member> list = memberDao.getList(size, offset, keyword, option);
 		return list;
 	}
 
 	@Override
-	public int count(String keyword, String option) {
+	public int countMember(String keyword, String option) {
 		int count = memberDao.countBySearch(keyword, option);
 		return count;
+	}
+
+	@Override
+	public List<AdminMemberEvalDto> getEvalList(int page, String keyword, String option) {
+		int size = 10;
+		int offset=(page-1)*size;
+		List<AdminMemberEvalDto> list = memberEvalDao.getAdminViewList(size, offset, keyword, option);
+		return list;
+	}
+	
+	public int countEval(String keyword, String option) {
+		int count = memberEvalDao.countBySearch(keyword, option);
+		return count;
+	}
+
+	@Override
+	public int countParticipation(String keyword, String option) {
+		int count = participationDao.countBySearch(keyword, option);
+		return count;
+	}
+
+	@Override
+	public List<AdminParticipationDto> getParticipationList(int page, String keyword, String option) {
+		int size = 10;
+		int offset=(page-1)*size;
+		List<AdminParticipationDto> list = participationDao.getAdminViewList(size, offset, keyword, option);
+		return list;
 	}
 	
 }
