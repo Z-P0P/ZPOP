9e0da521db056c68e13038bb3e92088c3b59df88
a-ryang <chnwu.kim@gmail.com>
2023-01-02 23:52:53 +0900

    !BREAKING CHANGE: get meeting detail
    * 컨트롤러에 있던 업무 로직 최대한 서비스로 옮김
    * 조회수 증가, startedAt 지남 여부에 따른 UPDATE때문에 Transactional 추가
    * 시작날짜, view단 태그들 응답에 추가 함

--- src/main/java/com/zpop/web/controller/MeetingController.java
+++ src/main/java/com/zpop/web/controller/MeetingController.java
@@ -7,6 +7,7 @@ import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
 
+import com.zpop.web.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
@@ -21,10 +22,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.zpop.web.dto.MeetingDetailDto;
-import com.zpop.web.dto.MeetingParticipantsDto;
-import com.zpop.web.dto.RegisterMeetingRequest;
-import com.zpop.web.dto.RegisterMeetingResponse;
 import com.zpop.web.entity.comment.CommentView;
 import com.zpop.web.security.ZpopUserDetails;
 import com.zpop.web.service.CommentService;
@@ -53,15 +50,15 @@ public class MeetingController {
 	@PostMapping("/register")
 	@ResponseBody
 	public String register(int categoryId, int regionId, int ageRangeId,
-			 int contactTypeId,	 int genderCategory, String title, String content,
-			 String detailRegion,int maxMember, @DateTimeFormat(pattern= "yyyy-MM-dd'T'HH:mm") LocalDateTime startedAt, String contact, @Nullable List <MultipartFile> images, 
-			 @AuthenticationPrincipal ZpopUserDetails userDetails,
-			 HttpServletRequest request) throws FileNotFoundException, IOException {
+				int contactTypeId,	 int genderCategory, String title, String content,
+				String detailRegion,int maxMember, @DateTimeFormat(pattern= "yyyy-MM-dd'T'HH:mm") LocalDateTime startedAt, String contact, @Nullable List <MultipartFile> images, 
+				@AuthenticationPrincipal ZpopUserDetails userDetails,
+				HttpServletRequest request) throws FileNotFoundException, IOException {
 		
 		// 저장되는 파일 경로를 controller에서 얻어서 service로 넘겨줌
 		String path = "/images";
 		String realPath = request.getServletContext().getRealPath(path);
-		   
+
 		
 		int regMemberId = userDetails.getId();
 		System.out.println(regMemberId);
@@ -98,33 +95,15 @@ public class MeetingController {
 	@GetMapping("/{id}")
 	public String detailView(@PathVariable int id, Model model, 
 			@AuthenticationPrincipal ZpopUserDetails userDetails) {
-		MeetingDetailDto dto = service.getById(id);
-		List<MeetingParticipantsDto> participants = service.getParticipants(id);
-		
-		int memberId = 0;
-		int userType = 0;
-		if(userDetails!=null) {
+		Integer memberId = null;
+
+		if(userDetails != null)
 			memberId = userDetails.getId();
-			userType = service.getUserType(memberId,id);
-		}
-		
-		model.addAttribute("dto", dto);
-		model.addAttribute("participants", participants);
-		model.addAttribute("meetingId", id); 
-		model.addAttribute("userType",userType);
-		// service의 public 메서드 -> 사용자가 쓰는 기능
-		// 따라서 private updateViewCount으로 바꾸고 getById안에 넣기.
-		service.updateViewCount(id); // 조회수 증가 
-		
-		/*------------------------ 댓글 부분 ---------------------------*/
-		List<CommentView> comments = null;
-		if(memberId != 0)
-			comments = commentService.getCommentWithWriter(memberId, id);
-		else 
-			comments = commentService.getComment(id);
-		int countOfComment = commentService.getCountOfComment(id);
-		model.addAttribute("comments", comments);
-		model.addAttribute("countOfComment", countOfComment);
+
+		MeetingDetailResponse meetingDetail = service.getById(id, memberId);
+
+		model.addAttribute("meeting", meetingDetail);
+
 		return "meeting/detail";
 	}
 	
@@ -132,8 +111,8 @@ public class MeetingController {
 	@GetMapping("{meetingId}/participant")
 	@ResponseBody
 	public List<MeetingParticipantsDto> getParticipant(@PathVariable int meetingId){
-		List<MeetingParticipantsDto> list = service.getParticipants(meetingId);
-		return list;
+		// List<MeetingParticipantsDto> list = service.getParticipants(meetingId);
+		return null;
 	}
 	
 	//참여 AJAX endpoint (js에서 콜하는 함수)
