d11d1bad20cc684c6576209438fd9d531c875cf8
jin <apple380g@gmail.com>
2023-01-05 23:44:47 +0900

    - template> rate.html & js> profile-edit.js 삭제 됨 - ReplyController conflict (changed from origin/dev) - 멤버 프로필 모달에 사용할 임시 이미지 추가 image>minji.jpg - 멤버 프로필 모달에 사용할 아이콘 추가

--- src/main/java/com/zpop/web/controller/ReplyController.java
+++ src/main/java/com/zpop/web/controller/ReplyController.java
@@ -1,29 +1,19 @@
 package com.zpop.web.controller;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.zpop.web.entity.ReportedComment;
 import com.zpop.web.entity.comment.Comment;
 import com.zpop.web.entity.comment.CommentView;
 import com.zpop.web.security.ZpopUserDetails;
 import com.zpop.web.service.CommentService;
 import com.zpop.web.service.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/reply")
@@ -32,35 +22,35 @@ public class ReplyController {
 	private CommentService service;
 	@Autowired
 	private ReportService reportService;
-	
+
 	//답글(reply)불러오기 AJAX endpoint (js에서 콜하는 함수)
 	@GetMapping()
 	@ResponseBody
 	public  Map<String, Object> getReply(@RequestParam int groupId,
-			@AuthenticationPrincipal ZpopUserDetails userDetails) {
+										 @AuthenticationPrincipal ZpopUserDetails userDetails) {
 		List<CommentView> replies = null;
 		int memberId = 0;
-		if(userDetails!=null) 
+		if(userDetails!=null)
 			memberId = userDetails.getId();
 		if(memberId != 0)
 			replies = service.getReplyWithWriter(memberId, groupId);
 		else
 			replies = service.getReply(groupId);
 		int countOfReply = service.getCountOfReply(groupId);
-		
+
 		Map<String,Object> dto = new HashMap<>();
 		dto.put("status",200);
 		dto.put("resultObject",replies);
 		dto.put("countOfReply",countOfReply);
-		
+
 		return dto;
 	}
 
 	//답글(reply) 등록 AJAX endpoint (js에서 콜하는 함수)
 	@PostMapping()
 	@ResponseBody
-	public  String registerReply(@RequestBody Comment reply, 
-			@AuthenticationPrincipal ZpopUserDetails userDetails) {
+	public  String registerReply(@RequestBody Comment reply,
+								 @AuthenticationPrincipal ZpopUserDetails userDetails) {
 		reply.setWriterId(userDetails.getId());
 		service.registerReply(reply);
 		return "{\"1\":1}"; //JSON
@@ -69,8 +59,8 @@ public class ReplyController {
 	@PatchMapping("{id}")
 	@ResponseBody
 	public int updateReply(@PathVariable int id, @RequestBody Comment editedReply,
-			@AuthenticationPrincipal ZpopUserDetails userDetails) {
-		service.updateComment(editedReply);
+						   @AuthenticationPrincipal ZpopUserDetails userDetails) {
+		service.updateComment(editedReply, userDetails.getId());
 		Comment reply = service.getCommentById(id);
 		return reply.getGroupId();
 	}
@@ -78,8 +68,8 @@ public class ReplyController {
 	@DeleteMapping("{id}")
 	@ResponseBody
 	public int deleteReply(@PathVariable int id,
-			@AuthenticationPrincipal ZpopUserDetails userDetails) {
-		service.deleteComment(id);
+						   @AuthenticationPrincipal ZpopUserDetails userDetails) {
+		service.deleteComment(id, userDetails.getId());
 		Comment reply = service.getCommentById(id);
 		return reply.getGroupId();
 	}
@@ -87,7 +77,7 @@ public class ReplyController {
 	@PutMapping("{id}")
 	@ResponseBody
 	public String reportReply(@PathVariable int id, @RequestBody ReportedComment reportedReply,
-			@AuthenticationPrincipal ZpopUserDetails userDetails) {
+							  @AuthenticationPrincipal ZpopUserDetails userDetails) {
 		Comment reply = service.getCommentById(id);
 		reportedReply.setReporterId(userDetails.getId());
 		reportedReply.setOriginal(reply.getContent());
