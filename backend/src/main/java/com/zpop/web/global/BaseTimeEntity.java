e8c8c1da2487a4df2e0d58faa44d8b22f0737378
a-ryang <chnwu.kim@gmail.com>
2023-01-10 23:30:46 +0900

    feat: BaseTimeEntity 추가
    * 상속받은 엔티티들은 자동으로 업데이트되는 createdAt, updatedAt 컬럼이 생긴다

---
+++ backend/src/main/java/com/zpop/web/global/BaseTimeEntity.java
@@ -0,0 +1,23 @@
+package com.zpop.web.global;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+    @Column(updatable = false, nullable = false)
+    @CreatedDate
+    protected LocalDateTime createdAt;
+
+    @LastModifiedDate
+    protected LocalDateTime updatedAt;
+}
