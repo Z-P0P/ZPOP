613269900c0651e140cdad169007dcc3e16410e3
a-ryang <chnwu.kim@gmail.com>
2023-01-12 00:43:29 +0900

    feat: LoadingRoller.vue

---
+++ src/main/vue/src/components/LoadingRoller.vue
@@ -0,0 +1,108 @@
+<script>
+export default {
+  name: "LoadingRoller",
+  props: {
+    isShow: Boolean,
+  },
+};
+</script>
+
+<template>
+  <div v-if="isShow" class="loading-roller">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<style scoped>
+.loading-roller {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.loading-roller div {
+  animation: loading-roller 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  transform-origin: 40px 40px;
+}
+.loading-roller div:after {
+  content: " ";
+  display: block;
+  position: absolute;
+  width: 7px;
+  height: 7px;
+  border-radius: 50%;
+  background: var(--dark-grey1);
+  margin: -4px 0 0 -4px;
+}
+.loading-roller div:nth-child(1) {
+  animation-delay: -0.036s;
+}
+.loading-roller div:nth-child(1):after {
+  top: 63px;
+  left: 63px;
+}
+.loading-roller div:nth-child(2) {
+  animation-delay: -0.072s;
+}
+.loading-roller div:nth-child(2):after {
+  top: 68px;
+  left: 56px;
+}
+.loading-roller div:nth-child(3) {
+  animation-delay: -0.108s;
+}
+.loading-roller div:nth-child(3):after {
+  top: 71px;
+  left: 48px;
+}
+.loading-roller div:nth-child(4) {
+  animation-delay: -0.144s;
+}
+.loading-roller div:nth-child(4):after {
+  top: 72px;
+  left: 40px;
+}
+.loading-roller div:nth-child(5) {
+  animation-delay: -0.18s;
+}
+.loading-roller div:nth-child(5):after {
+  top: 71px;
+  left: 32px;
+}
+.loading-roller div:nth-child(6) {
+  animation-delay: -0.216s;
+}
+.loading-roller div:nth-child(6):after {
+  top: 68px;
+  left: 24px;
+}
+.loading-roller div:nth-child(7) {
+  animation-delay: -0.252s;
+}
+.loading-roller div:nth-child(7):after {
+  top: 63px;
+  left: 17px;
+}
+.loading-roller div:nth-child(8) {
+  animation-delay: -0.288s;
+}
+.loading-roller div:nth-child(8):after {
+  top: 56px;
+  left: 12px;
+}
+@keyframes loading-roller {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
