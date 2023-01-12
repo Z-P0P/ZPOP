54b458850e287965bf74f84d9da2cb2bad0d4508
a-ryang <chnwu.kim@gmail.com>
2023-01-12 22:29:01 +0900

    refactor: vue-router 계층형으로 변경

--- src/main/vue/src/router/index.js
+++ src/main/vue/src/router/index.js
@@ -4,20 +4,39 @@ import adminRoute from "./admin";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    adminRoute,
     {
       path: "/",
-      component: () => import("@/views/meeting/List.vue"),
+      component: () => import("@/views/Layout.vue"),
+      children: [
+        {
+          path: "login/oauth/:pathMatch(.*)*",
+          component: () => import("@/views/meeting/Login.vue"),
+        },
+        {
+          path: "meeting",
+          children: [
+            {
+              path: "list",
+              component: () => import("@/views/meeting/List.vue"),
+              alias: "/",
+            },
+            {
+              path: ":id(\\d+)", // matches only numbers
+              component: () => import("@/views/meeting/Detail.vue"),
+            },
+            {
+              path: "register",
+              component: () => import("@/views/meeting/Register.vue"),
+            },
+            {
+              path: "update/:id(\\d+)",
+              component: () => import("@/views/meeting/Update.vue"),
+            },
+          ],
+        },
+      ],
     },
-    adminRoute,
-    { path: "/login/oauth/:pathMatch(.*)*", component: () => import ("@/views/meeting/Login.vue")},
-    { path: '/meeting/register', component: () => import("@/views/meeting/Register.vue")},
-    { path: '/meeting/update/:id(\\d+)', component: () => import("@/views/meeting/Update.vue")},
-    
-    {
-      path: "/meeting/:id(\\d+)", // matches only numbers
-      component: () => import ("@/views/meeting/Detail.vue")
-    
-  },
   ],
 });
 
