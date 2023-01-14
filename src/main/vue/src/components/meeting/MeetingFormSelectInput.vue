14307a7b0287a1ffc37811ef0970ae59113ad184
Kyunghwan Kim <kkh147.17.3@gmail.com>
2023-01-14 22:24:52 +0900

    add: add component used for registering meeting

---
+++ src/main/vue/src/components/meeting/MeetingFormSelectInput.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="meeting-form__input">
+        <label class="input__label">{{ input.title }}</label>
+        <SelectBox @selectBoxClick="selectBoxClickhandler" 
+        @optionClick="optionClickhandler"
+        :option="input"/>
+        <span class="input__message" :class="{'input__message--appear': input.hasError}">{{ input.message }}</span>
+    </div>
+</template>
+
+<script>
+import SelectBox from './SelectBox.vue';
+
+export default {
+    props: ['input', 'store'],
+    components: { SelectBox },
+    emits : ['selectBoxClick','optionClick'],
+    setup(props,context){
+        const selectBoxClickhandler = (parameterName) =>{
+            context.emit('selectBoxClick',parameterName);
+        }
+        const optionClickhandler = (parameterName, placeholder, value) => {
+            context.emit('optionClick',parameterName, placeholder, value)
+        }
+        return{
+            selectBoxClickhandler,
+            optionClickhandler,
+        }
+    }
+}
+</script>
+
+<style scoped>
+.error{
+    
+}
+/* 
+.meeting-form__input{
+    display: flex;
+    flex-direction: column;
+}
+
+.input__label{
+    font-size: 1.125rem;
+    font-weight: var(--bold);
+}
+
+.input__message{
+	display:inline-flex;
+    align-items:center;
+    padding:5px 8px;
+    height:32px;
+} */
+</style>
+
---
+++ src/main/vue/src/components/meeting/MeetingFormTextInput.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="meeting-form__input">
+        <label class="input-text__label" for="">{{input.title}}</label>
+        <div class="input-text">
+            <div class="input-text__content-wrapper" :class="{'input-text__content-wrapper--error' : input.hasError}">
+                <input class="input-text__content" type="text" :placeholder="input.placeholder"
+                    :name="input.parameterName" @input="textInputHandler">
+            </div>
+        </div>
+        <span class="input__message" :class="{'input__message--appear' : input.hasError}">{{ input.message }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['input','store'],
+    emits:['textInput'],
+    setup(props, context){
+        const store = props.store;
+        const textInputHandler = (e) => {
+            const parameterName = props.input.parameterName;
+            context.emit('textInput', parameterName, e.target.value);
+        }
+        return {
+            store,
+            textInputHandler,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
