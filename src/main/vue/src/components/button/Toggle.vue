1fb3df536fb15a93daad7783a1230bdab8e9db26
a-ryang <chnwu.kim@gmail.com>
2023-01-17 15:05:33 +0900

    feat: Toggle component

---
+++ src/main/vue/src/components/button/Toggle.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <input type="checkbox" id="toggle" checked hidden />
+  <label for="toggle" class="toggle-switch">
+    <span class="toggle-button"></span>
+  </label>
+</template>
+
+<style></style>
