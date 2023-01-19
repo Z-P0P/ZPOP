176581868a1db320d33c48fdf68ad94929b4f7fd
jin <apple380g@gmail.com>
2023-01-19 21:47:51 +0900

    cleanup: clean up code Edit.vue and MyMeeting.vue

--- src/main/vue/src/views/member/Edit.vue
+++ src/main/vue/src/views/member/Edit.vue
@@ -1,13 +1,13 @@
 
 <script setup>
-import { reactive } from "vue";
-import api from "@/api";
-import ProfileEdit from "@/components/member/ProfileEdit.vue";
+	import { reactive } from "vue";
+	import api from "@/api";
+	import ProfileEdit from "@/components/member/ProfileEdit.vue";
 
-const state = reactive({
+	const state = reactive({
       myInfo: null,
+     
     });
-    console.log('myPage 설정중');
     // 모임 정보 조회 reactive는 view단과 model단 일치
     const getMyProfile = async () => {
       try {
@@ -24,12 +24,14 @@ const state = reactive({
     };
 
     getMyProfile();
+
 </script>
 <template>
   <div class="content-wrap">
   <ProfileEdit :my-info="state.myInfo"/>
   </div>
 </template>
+
 <style scoped>
    .content-wrap{
     padding: 20px;
