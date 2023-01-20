dfec582309236bb3810e609a184475ac5cc9cfc1
a-ryang <chnwu.kim@gmail.com>
2023-01-20 18:19:44 +0900

    feat: add 404 handle Detail.vue

--- src/main/vue/src/views/meeting/Detail.vue
+++ src/main/vue/src/views/meeting/Detail.vue
@@ -1,19 +1,19 @@
-
 <!-- detail vue = 화면 / article 화면의 구성 요소중 한 부분-->
 <script>
-import { reactive,ref } from "vue";
+import { reactive, ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import api from "@/api"; //index.js
 import Article from "@/components/meeting/Article.vue";
 import Participants from "@/components/meeting/Participants.vue";
 import CommentList from "@/components/comment/CommentList.vue";
+import { ServerException } from "@/utils/ServerException";
 
 export default {
   name: "MeetingDetail",
   components: {
-    'Article':Article,
-    'Participants':Participants,
-    'CommentList':CommentList,
+    Article: Article,
+    Participants: Participants,
+    CommentList: CommentList,
   },
   setup() {
     const state = reactive({
@@ -21,51 +21,53 @@ export default {
     });
     // 모임 정보 조회 reactive는 view단과 model단 일치
     const route = useRoute();
-    const getDetail = () => {
+    const router = useRouter();
+
+    const getDetail = async () => {
       try {
-        api.meeting
-            .getDetail(route.params.id)
-            .then(res => res.json())
-            .then(data => {state.detail = data});
+        const res = await api.meeting.getDetail(route.params.id);
+        if (!res.ok) throw new ServerException(res);
+        const data = await res.json();
+        state.detail = data;
       } catch (e) {
-        console.log(e);
-        alert("잠시 후에 다시 시도해주세요");
+        // 존재하지 않는 meeting id
+        if (e.res.status === 404) router.push("/404");
       }
     };
+
     getDetail();
 
-    const newComment = () =>{
-      getDetail(route.params.id);
-    }
-    function increaseCounter(){
+    const newComment = async () => {
+      await getDetail(route.params.id);
+    };
+    function increaseCounter() {
       state.detail.commentCount++;
     }
-    
-    return { state,newComment,getDetail, increaseCounter}; //script에서 return된 값이 model
-  }
 
-}
+    return { state, newComment, getDetail, increaseCounter }; //script에서 return된 값이 model
+  },
+};
 </script>
 <template>
   <div class="content-wrap">
-  <Article :article="state.detail"/>
-  <Participants :detail="state.detail"/> 
-  <CommentList :detail="state.detail" @newComment="newComment" @counterIncreased="increaseCounter"/> 
-  
-
+    <Article :article="state.detail" />
+    <Participants :detail="state.detail" />
+    <CommentList
+      :detail="state.detail"
+      @newComment="newComment"
+      @counterIncreased="increaseCounter"
+    />
   </div>
 </template>
 <style scoped>
-       
-.content-wrap{
-  
+.content-wrap {
   max-width: 783px;
   width: 100%;
   padding: 1.2rem;
- }
-  @media (min-width: 576px) {
-.content-wrap{
-     padding: 4rem;
+}
+@media (min-width: 576px) {
+  .content-wrap {
+    padding: 4rem;
   }
 }
 </style>
