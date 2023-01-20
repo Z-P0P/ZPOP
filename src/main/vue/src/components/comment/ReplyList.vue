ddf2f48156bfc818c7bb6109d360e5f6259a7cdd
amy-pak <lihm2@naver.com>
2023-01-20 21:13:40 +0900

    fix: final fix for comment

--- src/main/vue/src/components/comment/ReplyList.vue
+++ src/main/vue/src/components/comment/ReplyList.vue
@@ -1,21 +1,20 @@
 <script setup>
-import { setMapStoreSuffix } from 'pinia';
-import { defineProps,defineEmits, reactive} from 'vue';
+import { defineProps,defineEmits} from 'vue';
 import { useCommentStore} from '@/stores/commentStore'
 import Reply from './Reply.vue'
 const props = defineProps({
   comment:Object
 });
-const emit = defineEmits(['counterIncreased']);
+const emit = defineEmits([
+  'counterIncreased'
+]);
 
-console.log(props.comment.id+" 리플라이리스트")
 const cmtStore = useCommentStore();
 const storedCommentId = cmtStore.comment.id;
 const commentId = props.comment.id;
 let replies = props.comment.replyList;
 if(storedCommentId != commentId){
   replies  = cmtStore.comment.replyList;
-console.log("피냐")
 }
 function increaseCounter(){
   emit('counterIncreased');
