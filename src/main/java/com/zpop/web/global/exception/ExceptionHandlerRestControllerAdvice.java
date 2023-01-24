8f2f10978b71be4a00fac070287a58a37cb2569c
a-ryang <chnwu.kim@gmail.com>
2023-01-25 00:14:35 +0900

    feat: exception handle

--- src/main/java/com/zpop/web/global/exception/ExceptionHandlerRestControllerAdvice.java
+++ src/main/java/com/zpop/web/global/exception/ExceptionHandlerRestControllerAdvice.java
@@ -1,15 +1,61 @@
 package com.zpop.web.global.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
 public class ExceptionHandlerRestControllerAdvice {
 
-    @ExceptionHandler(ResponseStatusException.class)
-    public ResponseEntity hanldeResponseStatusException(ResponseStatusException e) {
-        return new ResponseEntity(e.getMessage(), e.getStatusCode());
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponseBody> handleAllException(Exception e) {
+
+        ExceptionResponseBody errorResponse = ExceptionResponseBody.builder()
+                .status(INTERNAL_SERVER_ERROR.value())
+                .message("서버 에러")
+                .build();
+
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(errorResponse);
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ExceptionResponseBody> handleCustomException(CustomException e) {
+
+        ExceptionReason reason = e.getReason();
+
+        ExceptionResponseBody errorResponse = ExceptionResponseBody.builder()
+                .status(reason.getHttpStatus().value())
+                .message(reason.getMessage())
+                .build();
+
+        return ResponseEntity.status(reason.getHttpStatus()).body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponseBody> handleValidationException(MethodArgumentNotValidException e) {
+        // get all validation errors
+        BindingResult result = e.getBindingResult();
+        List<FieldError> fieldErrors = result.getFieldErrors();
+
+        List<String> details = new ArrayList<>();
+
+        for(FieldError error : fieldErrors)
+            details.add(error.getDefaultMessage());
+
+        ExceptionResponseBody errorResponse = ExceptionResponseBody.builder()
+                .status(BAD_REQUEST.value())
+                .message(ExceptionReason.VALIDATION_ERROR.getMessage())
+                .details(details)
+                .build();
+
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 }
