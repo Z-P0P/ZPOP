ba01b28be47570c8dc4dd6be6b0c827c298b8b29
jin <apple380g@gmail.com>
2023-01-30 18:02:23 +0900

    fix: form.css & select.css
    - 모임등록 페이지에서 해당 css 파일이 제대로 적용되지 않은 부분 (폰트 크기, 색상, 반응형 사이즈) 재적용

--- src/main/java/com/zpop/web/controller/HomeController.java
+++ src/main/java/com/zpop/web/controller/HomeController.java
@@ -1,26 +1,20 @@
 package com.zpop.web.controller;
 
-import java.io.IOException;
-import java.util.List;
-
+import com.zpop.web.dto.MeetingThumbnailResponse;
+import com.zpop.web.entity.MeetingFile;
+import com.zpop.web.service.MeetingService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.zpop.web.dto.MeetingThumbnailResponse;
-import com.zpop.web.entity.MeetingFile;
-import com.zpop.web.service.MeetingService;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import java.io.IOException;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -36,7 +30,7 @@ public class HomeController {
         this.meetingService = meetingService;
     }
 
-    @RequestMapping  // 루트 (index.html을 위해) 혹은 web 접근시에 Vue 프로젝트의 view 호출
+    @RequestMapping (value={"/","/{path:[^\\.]*}"}) // 루트 (index.html을 위해) 혹은 web 접근시에 Vue 프로젝트의 view 호출
     public String index() {
         return "index.html";
     }
